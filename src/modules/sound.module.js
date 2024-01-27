60201857d575117f1761997ebd0e59748bb135c1
Acron1k <danilm.5@yandex.ru>
2024-01-27 17:26:21 +0500

    Убрал длинный звук ГТА

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,7 +1,6 @@
 import {Module} from '../core/module'
 import bababooey from '../assets/sounds/bababooey.mp3'
 import brue from '../assets/sounds/brue.mp3'
-import gta from '../assets/sounds/gta.mp3'
 import napali from '../assets/sounds/na-nas-napali.mp3'
 import nope from '../assets/sounds/nope.mp3'
 import privet from '../assets/sounds/o-privet.mp3'
@@ -14,9 +13,9 @@ export class SoundModule extends Module {
 
     trigger() { // логику прописываем здесь, для расчёта используем файл utils.js
         console.log('sound')
-        const sounds = [bababooey,brue,gta,napali,nope,privet]
+        const sounds = [bababooey,brue,napali,nope,privet]
         const audio = new Audio(sounds[Math.floor(Math.random()*sounds.length)])
         audio.play()
-        
+
     }
 }
