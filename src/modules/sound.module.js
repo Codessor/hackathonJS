d9af5b5a10abce1ed5e7ef43c9e181cfd4953ebe
Acron1k <danilm.5@yandex.ru>
2024-01-27 15:35:32 +0500

    Реализовал воспроизведение рандомных звуков. Звуки лежат в папке assets/sounds. Не нашел способа как импортировать все разом, поэтмоу импортировал поштучно в файл modules/sound.module.js. Далее они помещаются в массив и при срабатывании триггера выбирается рандомный эдемент массива и проигрывается

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -1,4 +1,10 @@
 import {Module} from '../core/module'
+import bababooey from '../assets/sounds/bababooey.mp3'
+import brue from '../assets/sounds/brue.mp3'
+import gta from '../assets/sounds/gta.mp3'
+import napali from '../assets/sounds/na-nas-napali.mp3'
+import nope from '../assets/sounds/nope.mp3'
+import privet from '../assets/sounds/o-privet.mp3'
 
 export class SoundModule extends Module {
     constructor(type, text) {
@@ -7,5 +13,8 @@ export class SoundModule extends Module {
 
     trigger() { // логику прописываем здесь, для расчёта используем файл utils.js
         console.log('sound')
+        const sounds = [bababooey,brue,gta,napali,nope,privet]
+        const audio = new Audio(sounds[Math.floor(Math.random()*sounds.length)])
+        audio.play()
     }
 }
