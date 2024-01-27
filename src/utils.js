6c3751a556af90a48e194d8a35870e6f00446850
Codessor <FuriaWhite@yandex.ru>
2024-01-27 22:27:50 +0500

    Исправил ошибку модуля звука, нужно было добавить return у randomElementOfArray

--- src/utils.js
+++ src/utils.js
@@ -2,7 +2,7 @@ export function random(min, max) {
   return Math.round(min - 0.5 + Math.random() * (max - min + 1))
 }
 export function randomElementOfArray(array){
-  [Math.floor(Math.random()*array.length)]
+  return [Math.floor(Math.random()*array.length)]
 }
 export default function getRandomColor() {
   const letters = '0123456789ABCDEF';
