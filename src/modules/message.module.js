3c1eb7ccf0f9b56ba8163bf3bf291783c7362296
Codessor <FuriaWhite@yandex.ru>
2024-01-28 16:22:05 +0500

    Поменял стиль у модуля message

--- src/modules/message.module.js
+++ src/modules/message.module.js
@@ -1,6 +1,5 @@
 import { Module } from '../core/module'
 import { loremIpsum } from "lorem-ipsum"
-import getRandomColor from '../utils';
 
 
 export class MessageModule extends Module {
@@ -9,21 +8,21 @@ export class MessageModule extends Module {
   }
 
   trigger() { // логику прописываем здесь, для расчёта используем файл utils.js
+    const wrapper = document.createElement('div')
+    wrapper.className = 'message-wrapper'
     const textBlock = document.createElement('div')
     textBlock.className = 'message-block'
-    textBlock.style.width = "500"
-    textBlock.style.height = "300"
-    textBlock.style.background = getRandomColor()
     const text = document.createElement('p')
     textBlock.append(text)
+    wrapper.append(textBlock)
     text.textContent = `${loremIpsum()}`
-    document.body.prepend(textBlock)
+    document.body.prepend(wrapper)
     textBlock.style.opacity = 1
     setTimeout(() => {
       setInterval(() => {
         textBlock.style.opacity -= 0.05
         if (textBlock.style.opacity <= 0) {
-          textBlock.remove()
+          wrapper.remove()
         }
       }, 35)
     }, 4000)
